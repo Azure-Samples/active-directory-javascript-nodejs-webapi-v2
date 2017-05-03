3799acb7c84b6067a65b894ad45b7485165dcb35
danieldobalian <ddobalian@gmail.com>
2017-05-03 11:58:25 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// Authors:
+// Shane Oatman https://github.com/shoatman
+// Sunil Bandla https://github.com/sunilbandla
+// Daniel Dobalian https://github.com/danieldobalian
+
+var express = require("express");
+var morgan = require("morgan");
+var passport = require("passport");
+var BearerStrategy = require('passport-azure-ad').BearerStrategy;
+
+var options = {
+    identityMetadata: "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration/",
+    clientID: "YOUR_CLIENT_ID", // Replace with your Client ID
+    validateIssuer: false,
+    loggingLevel: 'warn',
+    passReqToCallback: false
+};
+
+// Check for client id placeholder
+if (options.clientID === 'YOUR_CLIENT_ID') {
+    console.error("Please update 'options' with the client id (application id) of your application");
+    return;
+}
+
+var bearerStrategy = new BearerStrategy(options,
+    function (token, done) {
+        // Send user info using the second argument
+        done(null, {}, token);
+    }
+);
+
+var app = express();
+app.use(morgan('dev'));
+
+app.use(passport.initialize());
+passport.use(bearerStrategy);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/hello",
+    passport.authenticate('oauth-bearer', {session: false}),
+    function (req, res) {
+        var claims = req.authInfo;
+        console.log('User info: ', req.user);
+        console.log('Validated claims: ', claims);
+        
+        res.status(200).json({'name': claims['name']});
+    }
+);
+
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Listening on port " + port);
+});
