cd5b5acb702c18a587cf55cd7951e4d3e4a7dc23
Dogan Erisen <dogan.erisen@gmail.com>
2020-03-13 10:45:33 -0700

    file name changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const passport = require("passport");
-const config = require('./apiConfig');
+const config = require('./config');
 const BearerStrategy = require('passport-azure-ad').BearerStrategy;
 
 // Check for clientId placeholder
